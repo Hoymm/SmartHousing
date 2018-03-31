cac7f99dc8c3e34fa4930b575fb82e113f1ccd65
Hoymm <Damian.Andrzej.Muca@gmail.com>
2018-03-31 13:40:28 +0200

    Add class to store house data

---
+++ src/HouseData.java
@@ -0,0 +1,5 @@
+public class HouseData {
+    private String owner;
+    private int number, squareMeters, solarPanelsAmount, electricityConsumptionkWhPerDay, waterConsumptionPerDay,
+            gasConsumptionLitresPerDay;
+}
